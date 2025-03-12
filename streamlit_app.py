0fa7d5ddb9341561513c334baf394127b9e394c5
Sterl0927 <sterling.perez@chase.com>
2025-03-12 08:33:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("Customize Your Smoothie!")
@@ -48,4 +49,4 @@ if ingredients_list:
 
 
 
-import requests
+
